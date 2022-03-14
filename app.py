110712b24389b6c3b01da7e61e76fba7369e1724
Gyan Prakash Tiwari <golu04822@gmail.com>
2022-03-14 17:08:00 +0530

    added html.html

--- app.py
+++ app.py
@@ -7,18 +7,29 @@ app = Flask(__name__)
 def hello_world():
     return "<h1>Hello, World!</h1>"
 
+# @app.route("/get_pdf", methods=["GET"])
+# def get_pdf():
+#     items = ["fb",1,2,3,"my string"]
+#     rendered = render_template("new.html")
+#     css = ['templates/new.css']
+#     pdf = pdfkit.from_string(rendered,css=css)
+
+#     response = make_response(pdf)
+#     response.headers['Content-Type']='application/pdf'
+#     response.headers['Content-Disposition']='inline;filename=report.pdf'
+#     return response
+
+
 @app.route("/get_pdf", methods=["GET"])
 def get_pdf():
-    items = ["fb",1,2,3,"my string"]
-    # rendered = render_template("index.html",name="gyan",mail="gyan@yahoo.com",items=items)
-    rendered = render_template("new.html")
-    css = ['templates/new.css']
-    pdf = pdfkit.from_string(rendered,False,css=css)
+    path = "/home/gyan/Desktop/gyan1/projects/pdfApiParams/templates/"
+    rendered = render_template("html.html",templates_path=path)
+    css = ['templates/css.css']
+    pdfkit.from_string(rendered,"output.pdf",css=css)
 
-    response = make_response(pdf)
-    response.headers['Content-Type']='application/pdf'
-    response.headers['Content-Disposition']='inline;filename=output.pdf'
-    return response
+    return "saved pdf in current directory"
+
+
 
 
 if __name__ == "__main__":
