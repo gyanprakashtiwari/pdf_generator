4f056a85cdc570e8042c8cbe3d9caf249f710213
Gyan Prakash Tiwari <golu04822@gmail.com>
2022-03-16 11:10:37 +0530

    dd

--- app.py
+++ app.py
@@ -1,40 +1,53 @@
-from flask import Flask ,render_template , request ,make_response
+from flask import Flask ,render_template , request ,make_response ,jsonify
+import requests
+import json
 import pdfkit
 
 app = Flask(__name__)
+
+def get_data_from_api(facility_type):
+    api_url = f"https://hz3tqftb8e.execute-api.ca-central-1.amazonaws.com/api/nbh/auth/{facility_type}?lng=-79.3709183&api_key=FES80v8F3r4rMQfoFT7NC4q1sthviz9077SRfatI&lat=43.7639275&client_id=9562fbc00d56cc95209b818e8f9040654cecd9f4"
+    response_data = requests.get(api_url)
+    return (response_data.content)
 
 @app.route("/")
 def hello_world():
     return "<h1>Hello, World!</h1>"
 
-# @app.route("/get_pdf", methods=["GET"])
-# def get_pdf():
-#     items = ["fb",1,2,3,"my string"]
-#     rendered = render_template("new.html")
-#     css = ['templates/new.css']
-#     pdf = pdfkit.from_string(rendered,css=css)
-
-#     response = make_response(pdf)
-#     response.headers['Content-Type']='application/pdf'
-#     response.headers['Content-Disposition']='inline;filename=report.pdf'
-#     return response
-
 
 @app.route("/get_pdf", methods=["GET"])
 def get_pdf():
-    path = "/home/gyan/Desktop/gyan1/projects/pdfApiParams/templates/"
-    rendered = render_template("html.html",templates_path=path)
-    css = ['templates/css.css']
-    pdfkit.from_string(rendered,"output.pdf",css=css)
+    path = "/home/gyan/Desktop/gyan1/projects/pdfApiParams/templates"
+    all_types = ["bar","daycare", "healthcare","ele_school","sec_school","grocery","cafe","restaurant","walkscore"]
+    # for all types of data dict/map
+    all_data = {}
+    try:
+        for t in all_types:
+            facility_type = t
+            data = get_data_from_api(facility_type)
+            data = json.loads(data.decode('utf-8'))
+            all_data[facility_type] = data[facility_type]
+        print(all_data["sec_school"])
+    except:
+        return {"msg":"unable to get all data from api"}
+    
+    
+    
+    try:
+        rendered = render_template("index.html",templates_path=path,data = all_data)
+        css = ['templates/main.css']
+        pdfkit.from_string(rendered,"output.pdf",css=css)
+    except:
+        return {"msg":"some error in generating pdf"}
+        # return all_data
 
-    return "saved pdf in current directory"
-
+    
+    return {"msg":"pdf report generated successfully"}
 
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
 
 
 
